139601bf9539eabb6fe12e58a6550f36be878033
azimsbaghadiya <38919150+azimsbaghadiya@users.noreply.github.com>
2018-06-17 16:24:21 -0400

    Update UserProfile.java

--- Source Code/Code Behind/UserProfile.java
+++ Source Code/Code Behind/UserProfile.java
@@ -11,18 +11,7 @@ public class UserProfile{
 	public int getUniqueId(){ return this.uniqueId; }
 	public int getMobile(){ return this.mobile; }
 	
-	public String[] getAddress(){
-		String [] addressArray = new String[5];
-		addressArray[0] = address.houseOrApt;
-		addressArray[1] = addres.street;
-		addressArray[2] = address.city;
-		addressArray[3] = address.province;
-		addressArray[4] = address.country;
-		addressArray[5] = address. zip;
-		
-		return addressArray;
-		
-	}
+	
 	
 	public UserProfile( String name, int uniqueId, int mobile, Address address, String[] interests ){
 		this.name = name;
